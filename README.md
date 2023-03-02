305b997130df89585cecebb1b1a6426b58493c37
anas-araid <araid.anas99@gmail.com>
2023-03-02 21:50:54 +0100

    refactor: added moveable and fixed canvas

--- README.md
+++ README.md
@@ -1,7 +1,27 @@
-# Visual Studio Code Infinite Worksapce
+##### ⚠️ Disclaimer
 
-## Development
+> This is just a work in progress proof of concept. Currently used only for experimental purposes so it's not optimized for production use, yet.
+
+# VS Code Infinite Workspace
+
+Prototype built in order to recreate a figma-like canvas experience in Visual Studio Code. 
+
+### Development
 
 - Clone this repository
 - Run `npm install` to install dependencies
 - Run `npm run watch` to start developing
+
+### TODO
+- initial children positioning
+- close window
+- save file
+- create new file
+- fix z-index/window focus issue
+- on window drag -> decrease scale -> on drag end -> restore scale
+
+### Contributing
+If you have any questions or requests or want to contribute, please write an issue or give me a PR freely.
+
+### Bug report
+If you find a bug, please report to us opening a new [issue](https://github.com/anas-araid/vscode-infinite-workspace/issues) on GitHub.
--- src/webview/App.tsx
+++ src/webview/App.tsx
@@ -1,28 +1,19 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect } from 'react';
 import { messageHandler } from '@estruyf/vscode/dist/client';
 import "./styles.css";
 import { COMMAND } from './constants';
 import { CustomFile, GetFile } from '../types';
 import styled from 'styled-components';
 import React = require('react');
-import { Target, useGesture, useWheel } from '@use-gesture/react';
 import { Window } from './components/window';
-import { mapFiles, supportedTypes } from './utils';
+import { mapFiles } from './utils';
 import InfiniteViewer from 'react-infinite-viewer';
 
 export interface IAppProps {}
 
 export const App: React.FunctionComponent<IAppProps> = ({ }: React.PropsWithChildren<IAppProps>) => {
-  const [{ scale, translateX, translateY, originX, originY }, setTransform] = React.useState({
-    scale: 1,
-    translateX: 0,
-    translateY: 0,
-    originX: 0,
-    originY: 0,
-  });
   const [cursor, setCursor] = React.useState('auto');
   const [files, setFiles] = React.useState<CustomFile[]>([]);
-  const container = useRef(null);
 
   useEffect(() => {
     messageHandler.request<GetFile[]>(COMMAND.GET_FILES).then((data) => {
@@ -34,51 +25,9 @@ export const App: React.FunctionComponent<IAppProps> = ({ }: React.PropsWithChil
   document.addEventListener('gesturestart', (e) => e.preventDefault());
   document.addEventListener('gesturechange', (e) => e.preventDefault());
 
-  const bind = useGesture({
-    onDrag: ({ delta: [dx, dy], event }) => {      
-      if (event.currentTarget !== event.target) {
-        return;
-      }
-      setTransform((prev) => ({
-        scale: prev.scale, //Math.max(prev.scale + dy / 100, 0.1),
-        translateX: prev.translateX + dx,
-        translateY: prev.translateY + dy,
-        originX: prev.originX,
-        originY: prev.originY,
-      }));
-    },
-    onPinch: ({ offset: [d], origin, event,  }) => {
-      if (event.currentTarget !== event.target) {
-        return;
-      }
-      console.log('origin', origin);
-      
-      setTransform((prev) => ({
-        scale: d,//Math.max(prev.scale + dy / 100, 0.1),
-        translateX: prev.translateX,
-        translateY: prev.translateY,
-        originX: origin[0],
-        originY: origin[1]
-      }));
-    },
-    onDragStart: ({event}) => {     
-      if (event.currentTarget !== event.target) {
-        return;
-      }
-      setCursor('grabbing');
-    },
-    onDragEnd: ({event}) => {
-      if (event.currentTarget !== event.target) {
-        return;
-      }
-      setCursor('auto');
-    },
-  }, {
-    target: container,
-    eventOptions: {passive: true}
-  });  
-
-
+  const calcTranslate = (index: number) => {
+    return { transform: `translate(${540 * index}px, 0px)`};
+  };
 
   const children = files.map((file, i) => {
     return (
@@ -86,115 +35,36 @@ export const App: React.FunctionComponent<IAppProps> = ({ }: React.PropsWithChil
         key={String(i)} 
         title={file.name} 
         content={file.content} 
-        fileType={file.type} 
+        fileType={file.type}
+        // style={calcTranslate(i)}
+        onDragStart={() => setCursor('grabbing')}
+        onDragEnd={() => setCursor('auto')}
       />
     );
   });
-
-  // const children = asdf.map((window, i) => {
-  //   return (
-  //     <React.Fragment key={i}>
-  //       {window}
-  //     </React.Fragment>
-  //   );
-  // });
-  console.log('files', files);
-  
-  console.log('children', children);
 
   if (children.length === 0) {
     return <></>;
   }
   
   return (
-    <Container>
+    <Container style={{cursor}}>
       <InfiniteViewer 
         className="viewer" 
         displayHorizontalScroll={false}
         displayVerticalScroll={false}
-        useAutoZoom
+        useAutoZoom={true}
+        useWheelScroll={true}
+        useMouseDrag={true}
+        wheelPinchKey='ctrl'
       >
-         {/* {
-          files.length > 0 && files.map((file, i) => {
-            return (
-              <Window 
-                key={String(i)} 
-                title={file.name} 
-                content={file.content} 
-                fileType={file.type} 
-              />
-            );
-          })
-        } */}
-
-        {children}
-        {/* <Window 
-          key={'asd'} 
-          title={'asdf'} 
-          content={'.'} 
-          fileType={'css'} 
-        /> */}
-        {/* <div style={{ backgroundColor: "red", height: 200, width: 200 }}>
-          AA
-        </div> */}
+        <div className="viewport">
+          {children}
+        </div>
       </InfiniteViewer>
     </Container>
-    // <Container>
-    //   <SottoContainer>
-    //     <InfiniteViewer className="viewer" style={{height: 200}}>
-    //     <div style={{ backgroundColor: "red", height: 200, width: 200 }}>
-    //           AA
-    //         </div>
-    //       {/* <Asdf>
-    //         AA
-    //       </Asdf> */}
-    //     </InfiniteViewer>
-    //   </SottoContainer>
-
-    // </Container>
   );
-
-  // return ( // {...bind()} style={{ cursor }}
-  //   <Container ref={container} style={{ cursor }}  >
-  //     <Canvas 
-  //       scale={scale} 
-  //       translateX={translateX} 
-  //       translateY={translateY}
-  //       originX={originX}
-  //       originY={originY}
-  //     >
-  //       {
-  //         files.length > 0 && files.map((file, i) => {
-  //           return (
-  //             <Window 
-  //               key={String(i)} 
-  //               title={file.name} 
-  //               content={file.content} 
-  //               fileType={file.type} 
-  //             />
-  //           );
-  //         })
-  //       }
-  //     </Canvas>
-  //   </Container>
-  // );
 };
-
-const Asdf = styled.div`
-  background-color: red;
-  height: 200px;
-  width: 200px;
-`;
-
-const Canvas = styled(InfiniteViewer)`
-  border: 2px solid red;
-`;
-
-const SottoContainer = styled.div`
-  border: solid 1px red;
-  height: 400px;
-  width: 400px;
-`;
 
 const Container = styled.div`
   display: flex;
@@ -204,58 +74,7 @@ const Container = styled.div`
   width: 100%;
   top: 0px;
   left: 0px;
-  border: 1px solid green;
 `;
-
-// const Container = styled.div`
-//   position: fixed;
-//   height: 100%;
-//   width: 100%;
-//   top: 0px;
-//   left: 0px;
-//   background-color: #1e1e1e;
-//   touch-action: none;
-// `;
-
-interface CanvasProps {
-  scale: number;
-  translateX: number;
-  translateY: number;
-  originX: number;
-  originY: number;
-}
-
-// const Canvas = styled.div<CanvasProps>`
-//   position: absolute;
-//   will-change: transform;
-//   /* transform-origin: 0 0; */
-//   transform: 
-//     scale(${(props) => props.scale}) 
-//     translate(${(props) => props.translateX}px, ${(props) => props.translateY}px);
-//   transform-origin: ${(props) => `${props.originX}px ${props.originY}px`};
-// `;
-
-
-
-  // useWheel(
-  //   ({ delta: [dx, dy], event, target: ref })  => {
-  //     event.preventDefault();
-  //     if (event.type === 'wheel') {
-  //       // console.log('dx', dx);
-  //       // console.log('dy', dy);
-        
-  //       setTransform((prev) => ({
-  //         scale: Math.max(prev.scale + dy / 100, 0.1),
-  //         translateX: prev.translateX + (dx * -1),
-  //         translateY: prev.translateY + (dy * -1),
-  //       }));
-  //     }
-  //   },
-  //   // we need to use a ref to be able to get non passive events and be able
-  //   // to trigger event.preventDefault()
-  //   { target: ref, eventOptions: { passive: false } }
-  // );
-
 
  // const [message, setMessage] = React.useState<string>("");
   // const [error, setError] = React.useState<string>("");
